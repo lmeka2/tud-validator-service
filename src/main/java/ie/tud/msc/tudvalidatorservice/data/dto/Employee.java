0b575ff3d3d678bba176e648b4bf90df2bc21504
lakshman <lakshman.1988@gmail.com>
2022-01-20 09:24:06 +0000

    added jpa repo

---
+++ src/main/java/ie/tud/msc/tudvalidatorservice/data/dto/Employee.java
@@ -0,0 +1,26 @@
+package ie.tud.msc.tudvalidatorservice.data.dto;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "Employee")
+public class Employee implements Serializable {
+    @Id
+    private long employeeId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private Date dateofbirth;
+
+    @Column
+    private String address;
+
+
+}
